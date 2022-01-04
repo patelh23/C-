318787f65326aa3858ec258cab66c084729656d4
Hardik Patel <patel.h23@gmail.com>
2022-01-03 23:53:15 -0500

    added solution to 2.41 example 1.22

--- Chapter 2/2.41/main.cpp
+++ Chapter 2/2.41/main.cpp
@@ -1,10 +1,5 @@
 #include <iostream>
-
-struct Sales_data {
-	std::string bookNo;
-	unsigned units_sold;
-	double revenue = 0.0;
-};
+#include "Sales_data.h"
 
 int main() {
 	Sales_data item1, item2;
@@ -29,4 +24,38 @@ int main() {
 	else
 		std::cout << "(no sales)" << std::endl;
 	return 0; //indicate success
+}
+
+/*
+ * Exercise 2.41: Use your Sales_data class to rewrite the exercises in § 1.5.1
+ * (p. 22), § 1.5.2 (p. 24), and § 1.6 (p. 25). For now, you should define your
+ * Sales_data class in the same file as your main function.
+ *
+ * Exercise 1.22: Write a program that reads several transactions for the same
+ * ISBN. Write the sum of all the transactions that were read.
+ *
+*/
+#include <iostream>
+#include "Sales_data.h"
+
+int main() {
+	Sales_data temp;
+	unsigned short transcount = 0;
+	double price = 0.0;
+
+	if (!(std::cin >> temp.bookNo >> temp.units_sold >> price)) {
+		std::cerr << "Invalid data";
+		return -1;
+	}
+	++transcount;
+	Sales_data total;
+	while (std::cin >> total.bookNo >> total.units_sold >> price) {
+		if (total.bookNo != temp.bookNo) {
+			std::cerr << "ISBN Mismatch" << std::endl;
+			return -1;
+		}
+		++transcount;
+	}
+	std::cout << "Number of transactions: " << transcount << std::endl;
+	return 0;
 }

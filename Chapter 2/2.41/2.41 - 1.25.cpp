80955feeabbc9715a0c3c399a1ff91e68deb488f
Hardik Patel <patel.h23@gmail.com>
2022-01-07 17:28:59 -0500

    began Chapter 3 Exercises

--- Chapter 2/2.41/2.41 - 1.25.cpp
+++ Chapter 2/2.41/2.41 - 1.25.cpp
@@ -15,11 +15,39 @@ int main() {
 	double price, averagePrice = 0.0;
 
 	if (std::cin >> total.bookNo >> total.units_sold >> price) {
+		total.revenue = total.units_sold * price;
 		Sales_data trans;
-		if (trans.bookNo == total.bookNo) {
-			total.
+		while (std::cin >> trans.bookNo >> trans.units_sold >> price) {
+			trans.revenue = trans.units_sold * price;
+			if (total.bookNo == trans.bookNo) {
+				total.units_sold += trans.units_sold;
+				total.revenue += trans.revenue;
+			}
+			else {
+				averagePrice = total.revenue / total.units_sold;
+				std::cout << total.bookNo << " "
+					<< total.units_sold << " "
+					<< total.revenue << " "
+					<< averagePrice << std::endl;
+				total.bookNo = trans.bookNo;
+				total.units_sold = trans.units_sold;
+				total.revenue = trans.revenue;
+			}
+
+			averagePrice = total.revenue / total.units_sold;
+			std::cout << total.bookNo << " "
+				<< total.units_sold << " "
+				<< total.revenue << " "
+				<< averagePrice << std::endl;
 		}
-
-
+	}
+	else {
+		std::cerr << "No data?!" << std::endl;
+		return -1;
 	}
 }
+
+
+
+
+
---
+++ Chapter3/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+/*Exercise 3.1
+int main()
+{
+	int sum = 0, val = 1;
+	// keep executing the while as long as val is less than or equal to 10
+	while (val <= 10) {
+		sum += val; // assigns sum + val to sum
+		++val; // add 1 to val
+	} cout << "Using namespace std" << endl;
+}
+
+int main()
+{
+	string line;
+	// read input a line at a time until end-of-file
+	while (cin >> line) {
+		auto len = line.size();
+		cout << line << len << endl;
+		return 0;
+	}
+}
+//Section 3.2
+
+int main()
+{
+	string line;
+	// read input a line at a time until end-of-file
+	while (getline(cin, line)){
+	cout << line << endl;
+	return 0;
+	}
+}
+
+
+//Section 3.3
+White spaces end the string input
+In the getLine function, it will collect all the spaces
+*/
+
+//Section 3.4
+int main() {
+	string word1;
+	string word2;
+	std::cin >> word1 >> word2;
+	if (word1 == word2)
+		std::cout << "These words are equal" << std::endl;
+	else
+		cout << ((word1.size() > word2.size())
+			? "Word 1 is bigger"
+			: "Word 2 is bigger") << std::endl;
+}

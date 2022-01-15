55aaed0e8170d84ef38007d5cbc1bea82534c26d
Hardik Patel <patel.h23@gmail.com>
2022-01-14 21:07:25 -0500

    added solution 3.13

--- Chapter3/main.cpp
+++ Chapter3/main.cpp
@@ -111,7 +111,7 @@ int main (){
 	cout << s[0] << endl;
 	//The following program prints out the first letter of the string.
 	//Yes, it is valid.
-*/
+
 //Solution 3.10
 int main() {
 	string s;
@@ -122,3 +122,43 @@ int main() {
 
 	}
 }
+
+//Exercise 3.11: Is the following range for legal ? If so, what is the type of c ?
+int main() {
+	const string s = "Keep out!";
+	for (auto& c : s) {
+		std::cout << c;
+	}
+}
+//c is a reference to the current index of s (char reference)
+
+//Exercise 3.12: Which, if any, of the following vector definitions are in
+error ? For those that are legal, explain what the definition does.For those
+that are not legal, explain why they are illegal.
+(a)vector<vector<int>> ivec;     //this defines a vector of vector elements of type int
+(b)vector<string> svec = ivec;   // this is in error because you cannot initialize int vector to string svec
+(c)vector<string> svec(10, "null");  //this will define svec a vector of 10 elements type string
+
+int main() {
+	vector<string> svec(10, "null");
+	for (auto& c : svec) {
+		std::cout << c;
+	}
+}
+(a)vector<int> v1;               //empty vector with no values
+(b)vector<int> v2(10);			  //vector with 10 elements, all at 0. 0000000000
+(c)vector<int> v3(10, 42);       //vector with 10 elements, all at 42
+(d)vector<int> v4{ 10 };			  //vector with 1 element, value of 10
+(e)vector<int> v5{ 10, 42 };       //vector with 2 elements, value of 42
+(f)vector<string> v6{ 10 };
+(g)vector<string> v7{ 10, "hi" };
+
+//Exercise 3.13
+int main() {
+
+	vector<int> v4{ 10,42 };
+	for (auto& c : v4) {
+		std::cout << c;
+	}
+}
+*/

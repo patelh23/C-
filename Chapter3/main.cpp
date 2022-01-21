68d0318f5cc96ea5d857235952717ec17a67c7a5
Hardik Patel <patel.h23@gmail.com>
2022-01-20 23:56:52 -0500

    solution 3.21

--- Chapter3/main.cpp
+++ Chapter3/main.cpp
@@ -268,6 +268,10 @@ int main() {
 */
 //Exercise 3.21
 int main() {
-	auto b = v.begin(), e = v.end();
 
+	vector<int> v{ 1,2,3,4,5,6,7,8,9 };
+	for (auto b = v.begin(); b != v.end(); b++) {
+		*b = *b * *b;
+		std::cout << *b << std::endl;
+	}
 }

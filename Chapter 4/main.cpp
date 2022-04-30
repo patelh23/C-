74d138b2102c00fa1715fe658b984d886bc3da76
Hardik Patel <patel.h23@gmail.com>
2022-04-29 23:37:25 -0400

    added Solution 4.1

---
+++ Chapter 4/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+//Exercise 4.1: What is the value returned by 5 + 10 * 20 / 2 ?
+This will be executed like 5 + ((10 * 20) / 2)
+so it will be 200 / 2 = 100 + 5 = 105
+
+//Exercise 4.2
+a. * (vec.begin())
+b. (*(vec.begin())) + 1
+

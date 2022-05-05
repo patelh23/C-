a470f98138c537e0fa65716d5481cd9a1a05c895
Hardik Patel <patel.h23@gmail.com>
2022-05-04 22:52:19 -0400

    updated 4.2 & 4.3

--- Chapter 4/main.cpp
+++ Chapter 4/main.cpp
@@ -7,8 +7,8 @@ This will be executed like 5 + ((10 * 20) / 2)
 so it will be 200 / 2 = 100 + 5 = 105
 
 //Exercise 4.2
-a. * (vec.begin())
-b. (*(vec.begin())) + 1 //member (.) first then begin() call then dereference then add 1
+a. * ((vec.begin)())
+b. ((*((vec.begin)()))) + 1) //member (.) first then begin() call then dereference then add 1
 
 //Exercise 4.3
 Order of evaluation for most of the binary operators is left
@@ -16,7 +16,8 @@ undefined to give the compiler opportunities for optimization.This strategy
 presents a trade - off between efficient code generation and potential pitfalls in
 the use of the language by the programmer.Do you consider that an
 acceptable trade - off ? Why or why not?
-//It is acceptable trade-off because a good programmer can utilize this optimization.
+// It is acceptable trade-off because a good programmer can utilize this optimization.
 // When in doubt, parenthesize expressions can be used to force the grouping that the
 // logic of your program requires.
-//Potential pitfalls can be fixed with static code analysis and other due diligence.
+// Potential pitfalls can be fixed with static code analysis and other due diligence.
+
